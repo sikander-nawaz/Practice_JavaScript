79d63a4bfe34ffa389a9d906898e5ad84eb54ba3
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-01 22:38:13 +0500

    'Add_Two_or_More_String'

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 console.log("Hello World");
 let userName = "Sikander Nawaz";
 console.log(userName);
+
+let firstName = "Sikander";
+let lastName = "Nawaz";
+let fullName;
+const userId = 2509;
+console.table([firstName,lastName,userId]);
+
+
+fullName = `Good Night! ${firstName} ${lastName}`;
+console.table([firstName,lastName,userId,fullName])
