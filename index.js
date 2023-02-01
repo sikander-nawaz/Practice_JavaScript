1a238ffb4a53c1d9324feedf89589062a17855fc
Khoof_Nak <sikandernawaz35@gmail.com>
2023-02-01 22:42:07 +0500

    Add_Labels

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+        // Practice
+
 console.log("Hello World");
 let userName = "Sikander Nawaz";
 console.log(userName);
 
+        // Combine OutPut
 let firstName = "Sikander";
 let lastName = "Nawaz";
 let fullName;
 const userId = 2509;
 console.table([firstName,lastName,userId]);
 
-
+        // Add_Two_or_More_String
 fullName = `Good Night! ${firstName} ${lastName}`;
 console.table([firstName,lastName,userId,fullName])
