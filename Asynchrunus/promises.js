b51d1d20b2b625e58ecb86e08f49b81c38ff86cf
JadoGarBhai <sikandernawaz35@gmail.com>
2023-06-28 15:12:41 +0500

    'm'

---
+++ Asynchrunus/promises.js
@@ -0,0 +1,12 @@
+var promise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve('hello world 1');
+        resolve('hello world 2');
+        resolve('hello world 3');
+        resolve('hello world 4');
+    }, 5000);
+});
+
+promise.then(function success(data) {
+    console.log(data);
+});
